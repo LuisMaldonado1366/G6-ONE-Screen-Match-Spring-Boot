b7283a8c6cddf1d925ce3e0aeee68e83f61ef182
luismartinez <luis.martinez@uelectronics.com>
2024-06-11 17:51:25 -0600

    Initial commit

---
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,34 @@
+package com.aluracursos.screenmatch;
+
+import com.aluracursos.screenmatch.service.APIConsume;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.net.URI;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	Dotenv dotenv = Dotenv.load();
+	String token = dotenv.get("OMDB_TOKEN");
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		APIConsume apiConsume = new APIConsume();
+		String url = "http://www.omdbapi.com/?apikey=" + token + "&t=" +
+				URLEncoder.encode("White collar", StandardCharsets.UTF_8);
+		String json = apiConsume.getData(url);
+		System.out.println(json);
+
+	}
+}
---
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/service/APIConsume.java
@@ -0,0 +1,23 @@
+package com.aluracursos.screenmatch.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class APIConsume {
+
+    public String getData(String url) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+
+        return response.body();
+    }
+
+}
